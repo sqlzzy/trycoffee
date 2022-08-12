8396443dd539db513df83eb6b6fe8a579df69dc4
Sergey Osipov <osipovsergei97@gmail.com>
2022-08-12 21:24:45 +0300

    Создает проект trycoffee

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,29 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  publicPath: process.env.NODE_ENV === 'production'
+    ? ''
+    : '/',
+  transpileDependencies: true,
+  pages: {
+    'index': {
+      entry: './src/pages/Home/index.js',
+      template: './src/pages/Home/index.html',
+      title: 'Home',
+      chunks: [ 'chunk-vendors', 'chunk-common', 'index' ]
+    },
+    'card': {
+      entry: './src/pages/CardPage/index.js',
+      template: './src/pages/CardPage/index.html',
+      title: 'Card',
+      chunks: [ 'chunk-vendors', 'chunk-common', 'card' ]
+    },
+    'method': {
+      entry: './src/pages/MethodPage/index.js',
+      template: './src/pages/MethodPage/index.html',
+      title: 'Method',
+      chunks: [ 'chunk-vendors', 'chunk-common', 'method' ]
+    }
+  },
+});
+
