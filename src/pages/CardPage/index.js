1a11da0a89ef0062750cf9e9290421ab0734129e
Sergey Osipov <osipovsergei97@gmail.com>
2022-08-19 00:22:43 +0300

    Удаляет файл style.less для страницы CardPage и перемещает стили в reset.less

--- src/pages/CardPage/index.js
+++ src/pages/CardPage/index.js
@@ -2,7 +2,6 @@ import {createApp} from 'vue'
 import App from './index.vue'
 import '@/assets/reset.less';
 import router from '@/router/index';
-import './style.less';
 
 createApp(App)
     .use(router)
