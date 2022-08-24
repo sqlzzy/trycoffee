5ebe4cbd4fbff172090899410257232ecb520883
Sergey Osipov <osipovsergei97@gmail.com>
2022-08-24 17:31:36 +0300

    Добавляет ссылку на гитхаб проекта в компоненте footer

--- src/components/footer-page/FooterPage.vue
+++ src/components/footer-page/FooterPage.vue
@@ -1,6 +1,9 @@
 <template>
     <footer class="footer">
         <p class="footer__text">2022-...</p>
+        <a class="footer__link" href="https://github.com/sqlzzy/trycoffee">
+          <img src="../../assets/icons/footer/icon-github.png" alt="icon-github" loading="lazy" class="footer__icon footer__icon_type_github">
+        </a>
     </footer>
 </template>
 
