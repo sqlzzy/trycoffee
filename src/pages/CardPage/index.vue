e5e17c6daaa89a5edb7f276f1098f6934f221bfb
Sergey Osipov <osipovsergei97@gmail.com>
2022-08-26 03:12:39 +0300

    Подключает меню к странице CardPage

--- src/pages/CardPage/index.vue
+++ src/pages/CardPage/index.vue
@@ -1,5 +1,8 @@
 <template>
   <header-page :link="true"/>
+  <menu-page
+    :methods="apisMethods"
+  />
   <main class="content">
     <info-coffee 
       :title="currentCard.title"
@@ -35,6 +38,7 @@ import InfoCoffee from '@/components/info-coffee/InfoCoffee.vue';
 import FooterPage from '@/components/footer-page/FooterPage.vue';
 import RecipeCoffee from '@/components/recipe-coffee/RecipeCoffee.vue';
 import NoteCoffee from '@/components/note-coffee/NoteCoffee.vue';
+import MenuPage from '@/components/menu-page/MenuPage.vue';
 
 export default {
   name: 'CardPage',
@@ -44,6 +48,7 @@ export default {
     RecipeCoffee,
     FooterPage,
     NoteCoffee,
+    MenuPage,
   },
 
   computed: {
