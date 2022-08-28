6e8a5eb6d8185fb030c70f520e0a9e3a589fb7c8
Sergey Razdolyanskiy <grafcorp01@gmail.com>
2022-08-28 13:03:13 +0300

    Add MenuButton

---
+++ src/components/MenuButton/MenuButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <button
+        :aria-label="buttonDescription"
+        class="menuButton"
+        :class="{'menuButton__active' : isActive}"
+        @click="isActive = !isActive"
+    />
+</template>
+
+<script>
+export default {
+    name: "MenuButton",
+    data() {
+        return {
+            isActive: true,
+        }
+    },
+    computed: {
+        buttonDescription() {
+            return this.isActive ? 'Закрыть меню' : 'Открыть меню'
+        }
+    }
+}
+</script>
+
+<style src="./style.less" lang="less" scoped />
